c4ace6e9b7e826cbfe426d24a419abaaa83525f0
Kyochul-Jang <jang128@purdue.edu>
2022-07-05 17:23:17 +0900

    Upload exercise skeleton code

--- Week_7/exercise/skeleton_code.js
+++ Week_7/exercise/skeleton_code.js
@@ -1,5 +1,17 @@
 // Question 1
-num1 = 12
-num2 = 14
+num1 = 12;
+num2 = 14;
+result = 0;
+while () {
+    /* ToDo: write codes for finding a LCM */
+}
 
-while ()
+console.log(`The answer is {result}`);
+
+// Question 2
+numbers = [84, 17, 53, 100, 27, 90, 98, 95, 88, 65, 72];
+/* ToDo: write codes for finding a mean of `numbers` */
+
+/* ToDo: write codes for finding a median of `numbers` */
+
+/* ToDo: write codes for finding a standard deviation of `numbers` */
