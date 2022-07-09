73355dbda21f5984b186e42ce748938328cc76c5
Kyochul-Jang <jang128@purdue.edu>
2022-07-09 09:36:52 +0900

    Edit

--- Week_7/exercise/skeleton_code.js
+++ Week_7/exercise/skeleton_code.js
@@ -7,7 +7,7 @@ while () {
 }
 
 console.log(`The answer is {result}`);
-
+    
 // Question 2
 numbers = [84, 17, 53, 100, 27, 90, 98, 95, 88, 65, 72];
 /* ToDo: write codes for finding a mean of `numbers` */
