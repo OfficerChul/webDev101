4d67836802d55e21c05eb665a938b0eeb4df7ce6
OfficerChul <jang128@purdue.edu>
2022-07-24 22:18:55 +0900

    Update github link

--- webdev101_app/src/components/Cards/Cards.js
+++ webdev101_app/src/components/Cards/Cards.js
@@ -7,25 +7,25 @@ export default function Cards() {
         <div className="cards-container">
             <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' title='Week 1' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 2' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/b_Week_2/Week_2.md' content='Orientation' title='Week 2' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 3' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/c_Week_3/Week_3.md' content='Orientation' title='Week 3' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 4' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/d_Week_4/Week_4.md' content='Orientation' title='Week 4' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 5' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/e_Week_5/Week_5.md' content='Orientation' title='Week 5' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 6' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/f_Week_6/Week_6.md' content='Orientation' title='Week 6' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 7' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/g_Week_7/Week_7.md' content='Orientation' title='Week 7' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 8' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/h_Week_8/Week_8.md' content='Orientation' title='Week 8' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 9' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/i_Week_9/Week_9.md' content='Orientation' title='Week 9' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 10' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/j_Week_10/Week_10.md' content='Orientation' title='Week 10' />
 
-            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 11' />
+            <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/k_Week_11/Week_11.md' content='Orientation' title='Week 11' />
 
             <CardItems url='https://github.com/OfficerChul/webDev101/blob/main/a_Week_1/Week_1.md' content='Orientation' title='Week 12' />
 
